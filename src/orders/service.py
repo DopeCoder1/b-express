bf1a0db9ac40a5832d2ca763543abd467ebe2542
DISA <dope.coder1@gmail.com>
2024-01-09 11:47:26 +0600

    Add new view schemas

--- src/orders/service.py
+++ src/orders/service.py
@@ -12,15 +12,18 @@ from src.orders.models import (OrderItems, Orders, OrderStatus, Payment,
 from src.orders.schemas import (OrderItemsCreateSchemas, OrderItemsOut,
                                 OrdersCreateSchemas, OrderViewSchemas,
                                 PaymentOut)
+from src.orders.utils import nested_serializer
 from src.users.schemas import UserViewSchemas
 from src.warehouse.models import Warehouse
 from src.warehouse.schemas import WarehouseOutShort
-
+from src.database import get_db
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
 class OrderService(BaseDao):
     class_name = Orders
 
-    async def create_order(self, payload: OrdersCreateSchemas, user: UserViewSchemas):
+    async def create_order(self, payload: OrdersCreateSchemas, user: UserViewSchemas, db: AsyncSession = Depends(get_db)):
         order = await OrderService.add({
             "creator": user.id,
             "warehouse_id": payload.warehouse_id,
@@ -36,69 +39,27 @@ class OrderService(BaseDao):
             "reciever_phone": payload.reciever_phone,
             "insurance": payload.insurance,
         })
-        payment = Payment(**payload.payment.dict(),
+        payment = Payment(**payload.payment.model_dump(),
                           order_id=order.id, payment_status=PaymentStatus.NOT_PAID.value)
-        session = async_session()
-        session.add(payment)
+        db.add(payment)
         order.payment = payment
         order.order_items = payload.order_items
         for order_item in payload.order_items:
             order_item = OrderItems(
-                **order_item.dict(), order_id=order.id, status=OrderStatus.CREATED.value)
-            session.add(order_item)
-        await session.commit()
-        return order
+                **order_item.model_dump(), order_id=order.id, status=OrderStatus.CREATED.value)
+            db.add(order_item)
+        await db.commit()
+        return await nested_serializer.serialize_by_id(order.id, user)
 
-    async def get_orders(self, user: UserViewSchemas) -> list[OrderViewSchemas]:
-        session = async_session()
-        orders = await session.execute(select(Orders))
-        orders = orders.scalars().all()
-        orders_out = []
+    async def get_orders(self, user: UserViewSchemas, db: AsyncSession = Depends(get_db)) -> list[OrderViewSchemas]:
+        orders = await OrderService.find_all({"creator": user.id})
+        response = []
         for order in orders:
-            warehouse = await session.execute(select(Warehouse).where(Warehouse.id == order.warehouse_id))
-            warehouse = warehouse.scalars().first()
+            response.append(await nested_serializer.serialize_by_id(order.id, user, db))
+        return response
 
-            direction = await session.execute(select(Directions).where(Directions.id == order.direction_id))
-            direction = direction.scalars().first()
-
-            arival_city = await session.execute(select(City).where(City.id == direction.arrival_city_id))
-            arival_city = arival_city.scalars().first()
-
-            departure_city = await session.execute(select(City).where(City.id == direction.departure_city_id))
-            departure_city = departure_city.scalars().first()
-
-            arrival_city_out = CityOut(**arival_city.__dict__)
-            departure_city_out = CityOut(**departure_city.__dict__)
-
-            direction_out = DirectionOut(**direction.__dict__,
-                                         arrival_city=arrival_city_out,
-                                         departure_city=departure_city_out)
-
-            payment = await session.execute(select(Payment).where(Payment.order_id == order.id))
-            payment = payment.scalars().first()
-
-            order_items = await session.execute(select(OrderItems).where(OrderItems.order_id == order.id))
-            order_items = order_items.scalars().all()
-
-            order_items_out = []
-            for order_item in order_items:
-                order_items_out.append(OrderItemsOut(**order_item.__dict__))
-
-            orders_out.append(OrderViewSchemas(
-                **order.__dict__,
-                warehouse=WarehouseOutShort(**warehouse.__dict__),
-                direction=direction_out,
-                payment=PaymentOut(**payment.__dict__),
-                diection=direction_out,
-                order_items=order_items_out
-            ))
-        return orders_out
-
-    async def get_order(self, id: int, user: UserViewSchemas) -> OrderViewSchemas:
-        order = await OrderService.find_one_or_none({"id": id})
-        if order and order.creator == user.id:
-            return order
-        raise PermissionDenied()
+    async def get_order(self, id: int, user: UserViewSchemas, db: AsyncSession = Depends(get_db)) -> OrderViewSchemas:
+        return await nested_serializer.serialize_by_id(id, user, db)
 
     async def delete_orders(self, id: int, user: UserViewSchemas):
         order = await OrderService.find_one_or_none({"id": id})
--- src/warehouse/router.py
+++ src/warehouse/router.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, Depends, status
+from src.database import get_db
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.users.auth import JWTBearer
 from src.users.models import Users
@@ -16,13 +18,13 @@ async def create_warehouse(payload: WarehouseCreateSchemas, user: Users = Depend
 
 
 @router.get("/warehouses", status_code=status.HTTP_200_OK, response_model=list[WarehouseViewSchemas])
-async def list_warehouses(user: Users = Depends(JWTBearer())):
-    return await warehouse_service.get(user)
+async def list_warehouses(user: Users = Depends(JWTBearer()), db: AsyncSession = Depends(get_db)):
+    return await warehouse_service.get(user, db)
 
 
 @router.get("/warehouses/{id}", status_code=status.HTTP_200_OK, response_model=WarehouseViewSchemas)
-async def get_warehouse(id: int, user: Users = Depends(JWTBearer())):
-    return await warehouse_service.get_one(id, user)
+async def get_warehouse(id: int, user: Users = Depends(JWTBearer()), db: AsyncSession = Depends(get_db)):
+    return await warehouse_service.get_one(id, user, db)
 
 
 @router.patch("/warehouses/{id}", status_code=status.HTTP_200_OK, response_model=WarehouseViewSchemas)
